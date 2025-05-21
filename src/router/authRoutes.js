3105a54bcc2f162334614ba8c271f725f618e5e6
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2025-05-21 20:15:59 +0200

    fix: uncomment LoginView and restore login route in authRoutes

--- src/router/authRoutes.js
+++ src/router/authRoutes.js
@@ -1,6 +1,6 @@
 import AuthLayout from '@/views/auth/AuthLayout.vue'
-// import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
 
 const authRoutes = {
   path: '/',
@@ -8,12 +8,12 @@ const authRoutes = {
   component: AuthLayout,
   redirect: { name: 'login' },
   children: [
-    // {
-    //   path: '/auth/login',
-    //   name: 'login',
-    //   component: LoginView,
-    //   meta: { title: 'LayerLogic | Login' },
-    // },
+    {
+      path: '/auth/login',
+      name: 'login',
+      component: LoginView,
+      meta: { title: 'LayerLogic | Login' },
+    },
     {
       path: '/auth/register',
       name: 'register',

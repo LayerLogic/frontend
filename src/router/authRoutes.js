cd1e21160666813f5e06c8ce38e36ea91a6ccd39
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2025-05-21 19:44:28 +0200

    fix: comment out loginview

--- src/router/authRoutes.js
+++ src/router/authRoutes.js
@@ -1,5 +1,5 @@
 import AuthLayout from '@/views/auth/AuthLayout.vue'
-import LoginView from '@/views/auth/LoginView.vue'
+// import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 
 const authRoutes = {
@@ -8,12 +8,12 @@ const authRoutes = {
   component: AuthLayout,
   redirect: { name: 'login' },
   children: [
-    {
-      path: '/auth/login',
-      name: 'login',
-      component: LoginView,
-      meta: { title: 'LayerLogic | Login' },
-    },
+    // {
+    //   path: '/auth/login',
+    //   name: 'login',
+    //   component: LoginView,
+    //   meta: { title: 'LayerLogic | Login' },
+    // },
     {
       path: '/auth/register',
       name: 'register',

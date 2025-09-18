1aa11ec67b9d9eba3ae59cf4882f869d4bd8877b
Mohammed Agha <83072153+aghsa97@users.noreply.github.com>
2025-09-18 14:04:14 +0200

    feat: add test details dialog and functionality for editing notes in TrialView

--- src/api/test.js
+++ src/api/test.js
@@ -6,3 +6,7 @@ import service from '@/services/axios'
 export function getTestsByTrialId(id) {
   return service.get(`/researcher/trials/${id}/tests`)
 }
+
+export function updateTest(testId, data) {
+  return service.patch(`/researcher/tests/${testId}`, data)
+}
